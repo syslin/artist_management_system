3e64dcc9caf9368526c82017e96d93b3fe60a4ec
sanima <saneemastha@gmail.com>
2024-04-12 02:08:21 +0545

    Removed the migration file

--- db/schema.rb
+++ db/schema.rb
@@ -14,29 +14,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_192619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "phone"
-    t.datetime "dob"
-    t.integer "gender", default: 0
-    t.string "address"
-    t.integer "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   create_table "artists", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "dob"
-    t.integer "gender", default: 1
+    t.integer "gender", default: 0
     t.string "address"
     t.integer "first_release_year"
     t.integer "no_of_albums_released"
@@ -53,6 +34,25 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_192619) do
     t.datetime "updated_at", null: false
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
-  
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "phone"
+    t.datetime "dob"
+    t.integer "gender", default: 1
+    t.string "address"
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "songs", "artists"
 end
